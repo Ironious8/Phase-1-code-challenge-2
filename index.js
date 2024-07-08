13115e2ff95f27902696cfeda4d55eb28dbd49d0
Ironious8 <tyronekariuki@gmail.com>
2024-07-08 08:40:12 +0300

    Modified changes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+//Maintaining an array of items
 let items=[clothes, shoes, snacks];
 items.forEach(Element => {
     console.log(Element);
 });
 
+// Function for adding items to a list
 function inputItem () {
     let ul=document.getElementById('My list');
     let candidate=document.getElementById('candidate')
@@ -11,17 +13,21 @@ function inputItem () {
     li.appendChild(document.createTextNode(candidate.value));
     ul.appendChild(li);
 }
+//Event listener for the add button
 document.querySelector('btn-add').addEventListener('click', inputItem);
 
-
+// Function for removing items from a list
 function clearList () {
     let ul=document.getElementById('My list');
     ul.remove();
 }
+// Event listener for the clear list button
 document.querySelector('btn-remove').addEventListener('click', clearList);
 
+// Function for marking items as purchased
 function markItem() {
     let ul=document.getElementById('My list');
   ul.toggle('purchased');
 }
+// Event listener for the mark button
 document.querySelector('btn-mark').addEventListener('click', markItem);
